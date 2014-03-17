3e30f564cd12718ec82d70a5848b49ac414a89de
Charlie Thomason <charlie.thomason@razorfish.com>
2014-03-17 16:45:01 -0500

    Modified homepage style and added Flickr loading text

--- js/flickr.js
+++ js/flickr.js
@@ -2,7 +2,11 @@ var $ = jQuery.noConflict();
 
 $.getJSON("http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=c378d72ba608bb9d22c0ee252c90d333&user_id=24941028@N07&per_page=1&format=json&jsoncallback=?", displayImages);
 
-function displayImages(data) {	
+function displayImages(data) {
+
+	if($('#flickr-loading').length) {
+		$('#flickr-loading').remove();
+	}
 	 //Loop through the results in the JSON array of data. The 'data.photos.photo' bit is what you are trying to 'get at'. i.e. this loop looks at each photo in turn.
 	$.each(data.photos.photo, function(i,item){
 		
@@ -11,7 +15,7 @@ function displayImages(data) {
 			linkURL = 'http://www.flickr.com/photos/recycledfilm/' + item.id + '/';
 		
 		//Uses this URL to creats a html 'img' tag.
-		htmlString = '<div class="flickr-item"><a class="thumb" href="' + linkURL + '" target="_blank" rel="nofollow"><img src="' + photoURL + '" /></a><h2><a href="' + linkURL + '" target="_blank" rel="nofollow">' + item.title + '</a></h2><p><a class="btn" href="' + linkURL + '" target="_blank" rel="nofollow">More Info</a><a class="btn secondary-btn enlarge-btn" href="' + photoURL + '">Enlarge</a></p></div>';
+		htmlString = '<div class="flickr-item"><a class="thumb" href="' + linkURL + '" target="_blank" rel="nofollow"><img src="' + photoURL + '" /></a><h2><a href="' + linkURL + '" target="_blank" rel="nofollow">' + item.title + '</a></h2><p><a class="btn" href="' + linkURL + '" target="_blank" rel="nofollow">More Info</a> <a class="btn secondary-btn enlarge-btn" href="' + photoURL + '">Enlarge</a></p></div>';
 		
 		//Adds this image to the 'images1' div.
 		$('#flickr-wrap').append(htmlString);
